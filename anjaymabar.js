17a5d067ea24c5a565944b0acdd2d7871b5c294c
Juan Haniful Kahfi <42096811+juanhkahfi@users.noreply.github.com>
2025-05-14 10:19:13 +0700

    Update anjaymabar.js generate 3 code in one command and saved file to .txt

--- anjaymabar.js
+++ anjaymabar.js
@@ -1,3 +1,4 @@
+const fs = require("fs"); //modul fs
 function anjaymabar() {
   const puki = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
   let hasilanjaymabar = "";
@@ -12,4 +13,13 @@ function anjaymabar() {
   return hasilanjaymabar;
 }
 
-console.log(anjaymabar());
+// Generate 3 kode dan simpan ke dalam array
+let result = "";
+for (let i = 0; i < 3; i++) {
+  result += anjaymabar() + "\n"; // Setiap kode dipisah dengan baris baru
+}
+
+// Menyimpan hasil ke dalam file "hasil_kode.txt"
+fs.writeFileSync("hasil_kode.txt", result);
+
+console.log('Kode acak berhasil disimpan di "hasil_kode.txt"');
